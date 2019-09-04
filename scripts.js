ab8f5316a3616948b64c10921c3b18c293874403
Rasmus Andersen <rasmuslundandersen@gmail.com>
2019-09-04 20:14:54 +0200

    Final
    DONE

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById("ok").addEventListener("click",function(){
+    
+    var inputText = (document.getElementById("tekst").value).toUpperCase();
+   
+    document.getElementById("tekst").value = inputText;
+    
+});
